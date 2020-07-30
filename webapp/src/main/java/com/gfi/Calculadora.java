909c7a6b527facf9477d4005e5c9a0b056d6f662
jlrevillam <jose-luis.revilla@gfi.world>
2020-07-30 12:22:59 +0000

    sonarqube y jacoco

--- webapp/src/main/java/com/gfi/Calculadora.java
+++ webapp/src/main/java/com/gfi/Calculadora.java
@@ -2,6 +2,9 @@ package com.gfi;
 
 public class Calculadora{
 
+    /**
+     * Funcion que calcula la suma de los dos numeros enteros proporcionados
+    **/
     public static int suma(int numero1, int numero2){
         return numero1+numero2; // No escribo el codigo
     }
